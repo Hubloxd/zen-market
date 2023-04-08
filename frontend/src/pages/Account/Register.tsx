d6bbcc72aebaafce6fc9ea266037b9dbc14a8617
Hubert <hjakobczuk@gmail.com>
2023-04-08 16:00:54 +0200

    Implement Register functionality 	modified:   frontend/src/pages/Account/Register.tsx

--- frontend/src/pages/Account/Register.tsx
+++ frontend/src/pages/Account/Register.tsx
@@ -1,25 +1,77 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+
 import Navbar from "../Navbar";
-import { useDocumentTitle } from "../../utils";
+import { getCookie, useDocumentTitle } from "../../utils";
+import { API_URL } from "../../constants";
 
 export function RegisterForm() {
   const [formData, setFormData] = useState({
     username: "",
+    email: "",
     password: "",
+    confirmPassword: "",
   });
   const [error, setError] = useState<null | string>(null);
   const navigate = useNavigate();
 
   const handleChange = (
-    key: "username" | "password",
+    key: "username" | "password" | "email" | "confirmPassword",
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     formData[key] = event.target.value;
     setFormData(formData);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    setError(null);
+    event.preventDefault();
+
+    const { username, email, password, confirmPassword } = formData;
+    const csrftoken = getCookie("csrftoken");
+
+    const validateInputs = () => {
+      let error = "";
+      if (!username) {
+        error += "Username must not be empty. ";
+      }
+      if (!email) {
+        error += "Email must not be empty. ";
+      }
+      if (!password) {
+        error += "Password must not be empty. ";
+      } else if (password !== confirmPassword) {
+        error += "Passwords do not match. ";
+      }
+      return error;
+    };
+
+    const error = validateInputs();
+    if (error) {
+      setError(error.trim());
+      return;
+    }
+    try {
+      const response = await fetch(API_URL + "users/", {
+        method: "POST",
+        headers: {
+          Cookie: `csrftoken=${csrftoken};`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      navigate('/account/login/')
+    } catch (error: any) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
 
   return (
     <form
@@ -38,14 +90,30 @@ export function RegisterForm() {
         placeholder="username"
       />
       <input
+        onChange={(e) => handleChange("email", e)}
+        type="email"
+        name="email"
+        id="email"
+        placeholder="email"
+      />
+      <input
         onChange={(e) => handleChange("password", e)}
         type="password"
         name="password"
         id="password"
         placeholder="password"
       />
+      <input
+        onChange={(e) => handleChange("confirmPassword", e)}
+        type="password"
+        name="confirm-password"
+        id="confirm-password"
+        placeholder="confirm password"
+      />
 
-      {error ? <div className="text-red-600 font-light">{error}</div> : null}
+      {error ? (
+        <div className="text-red-600 font-light w-64">{error}</div>
+      ) : null}
 
       <div className="flex flex-row w-full justify-between mt-5 items-center">
         <Link to={"/account/register"}>
