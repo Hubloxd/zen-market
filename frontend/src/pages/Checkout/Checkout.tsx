15e44a9d597704081d6c6ef66828e0f3f018d721
Hubert <hjakobczuk@gmail.com>
2023-04-14 01:34:43 +0200

    Refactor code to improve readability and maintainability
    - Rearrange imports in Checkout component
    - Improve variable and function naming
    - Format code with Prettier
    - Implement useMemo and React.memo
    	modified:   frontend/src/pages/Checkout/Checkout.tsx

--- frontend/src/pages/Checkout/Checkout.tsx
+++ frontend/src/pages/Checkout/Checkout.tsx
@@ -1,79 +1,84 @@
-import "./checkout.css";
+import React, { useContext, useEffect, useMemo } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { useContext } from "react";
 import { useDocumentTitle } from "../../utils";
-
 import { CartContext } from "../../contexts/cart";
+import { UserContext } from "../../contexts/user";
 
 import Navbar from "../Navbar";
-import { Link } from "react-router-dom";
+import "./checkout.css";
+
 
 export default function Checkout() {
+  const { isLogged } = useContext(UserContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLogged()) {
+      navigate("/account/");
+    }
+  }, [isLogged]);
+
   useDocumentTitle("Zen Market | Checkout");
   const { cart, removeItemFromCart, resetCart } = useContext(CartContext);
-  let sum = 0;
-  cart.forEach((element) => {
-    sum += element.price;
-  });
-  const Products = () => {
-    const list: Array<JSX.Element> = [];
+  const totalPrice = useMemo(() => {
+    return cart.reduce((acc, item) => {
+      const price = Number(item.discount_price ?? item.price);
+      return acc + price;
+    }, 0);
+  }, [cart]);
 
-    cart.forEach((element) =>
-      list.push(
-        <tr key={element.name}>
-          <td width={"128px"}>
-            <Link to={`/product/${element.id}/`}>
-              <img
-                className="mx-auto"
-                width={"128px"}
-                src={element.image_src}
-                alt=""
-              />
-            </Link>
-          </td>
-          <td className="px-3 capitalize">
-            <Link to={`/product/${element.id}/`}>
-              <p>{element.name}</p>
-            </Link>
-          </td>
-          <td>{element.price}</td>
-
-          <td className="w-1">
-            <button
-              onClick={() => {
-                removeItemFromCart(element);
-              }}
-            >
-              <svg
-                fill="#ff0000"
-                height="30px"
-                width="24px"
-                version="1.1"
-                id="Capa_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 460.775 460.775"
-                xmlSpace="preserve"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path>{" "}
-                </g>
-              </svg>
-            </button>
-          </td>
-        </tr>
-      )
+  const Products = React.memo(() => {
+    return (
+      <>
+        {cart.map((item) => (
+          <tr key={item.id}>
+            <td width="128px">
+              <Link to={`/product/${item.id}/`}>
+                <img
+                  className="mx-auto"
+                  width="128px"
+                  src={item.image_src}
+                  alt=""
+                />
+              </Link>
+            </td>
+            <td className="px-3 capitalize">
+              <Link to={`/product/${item.id}/`}>
+                <p>{item.name}</p>
+              </Link>
+            </td>
+            <td>{item.discount_price || item.price}</td>
+            <td className="w-1">
+              <button onClick={() => removeItemFromCart(item)}>
+                <svg
+                  fill="#ff0000"
+                  height="30px"
+                  width="24px"
+                  version="1.1"
+                  id="Capa_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 460.775 460.775"
+                  xmlSpace="preserve"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path>{" "}
+                  </g>
+                </svg>
+              </button>
+            </td>
+          </tr>
+        ))}
+      </>
     );
-
-    return <>{list}</>;
-  };
+  });
 
   return (
     <>
@@ -103,7 +108,7 @@ export default function Checkout() {
               <td colSpan={120}>
                 <div className="flex flex-col float-right px-64 text-right">
                   <div>Total Items: {cart.length}</div>
-                  <div>Total: ${sum.toFixed(2)}</div>
+                  <div>Total: ${totalPrice.toFixed(2)}</div>
                 </div>
               </td>
             </tr>
@@ -121,20 +126,20 @@ export default function Checkout() {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
                 <path
                   d="M8.91003 19.9201L15.43 13.4001C16.2 12.6301 16.2 11.3701 15.43 10.6001L8.91003 4.08008"
                   stroke="#FFF"
-                  stroke-width="1.5"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
               </g>
             </svg>
