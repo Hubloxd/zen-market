7e35f2c75a0f56ed927b70823e499480ce3f2135
Hubert <hjakobczuk@gmail.com>
2023-04-21 02:53:04 +0200

    Transaction implementation

--- backend/rest/urls.py
+++ backend/rest/urls.py
@@ -7,6 +7,7 @@ app_name = 'Rest'
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductsViewSet)
 router.register(r'users', views.UserViewSet)
+router.register(r'transactions', views.TransactionViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
